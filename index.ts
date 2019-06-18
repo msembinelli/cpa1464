21a5a0c3e12fa65ee756152f292ad2981c2bf4f9
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2019-06-18 13:07:42 -0600

    Fixes

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 // tslint:disable variable-name
 const Cpa1464File = require('./dist/src/Cpa1464File');
-module.exports = Cpa1464File;
+module.exports = {
+  Cpa1464File,
+};
 // tslint:enable variable-name
